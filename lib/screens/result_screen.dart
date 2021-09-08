8c8c2102e86f4f997e20a4f9027abe05aacd9f88
J-P Nurmi <jp.nurmi@canonical.com>
2021-09-08 15:16:05 +0200

    WIP

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -1,11 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import '../constants.dart';
 
-class ResultScreen extends StatelessWidget {
+class ResultScreen extends StatefulWidget {
   const ResultScreen({Key? key}) : super(key: key);
 
   static Widget create(BuildContext context) => const ResultScreen();
+
+  @override
+  _ResultScreenState createState() => _ResultScreenState();
+}
+
+class _ResultScreenState extends State<ResultScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 5), () {
+      Navigator.of(context).pushReplacementNamed(kHomeScreenRoute);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
