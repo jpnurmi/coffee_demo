5eab90c7b9071e980423860d70a4594203b6463a
J-P Nurmi <jp.nurmi@canonical.com>
2021-09-08 19:32:41 +0200

    Cloud screen & assets

--- lib/screens/cloud_screen.dart
+++ lib/screens/cloud_screen.dart
@@ -5,16 +5,17 @@ import '../constants.dart';
 import '../routes.dart';
 
 class CloudProvider {
-  const CloudProvider(this.asset, [this.title = '']);
-  final String asset;
-  final String title;
+  const CloudProvider({required this.vendor, required this.public});
+  final String vendor;
+  final bool public;
 }
 
 const kProviders = [
-  CloudProvider('k8s.png', 'Private Cloud'),
-  CloudProvider('google.svg'),
-  CloudProvider('azure.svg', 'Public Cloud'),
-  CloudProvider('aws.svg'),
+  CloudProvider(vendor: 'mk8', public: false),
+  CloudProvider(vendor: 'gcp', public: true),
+  CloudProvider(vendor: 'azu', public: true),
+  CloudProvider(vendor: 'aws', public: true),
+  CloudProvider(vendor: 'doc', public: true),
 ];
 
 class CloudScreen extends StatelessWidget {
@@ -25,38 +26,34 @@ class CloudScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(kPadding),
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Select a substrate',
-              style: Theme.of(context).textTheme.headline4,
+      body: Column(
+        children: <Widget>[
+          const SizedBox(height: kPadding),
+          Text(
+            'Select a substrate',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          const SizedBox(height: kSpacing),
+          const Text('Where do you want your apps stack to run?'),
+          const SizedBox(height: kPadding),
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                _CloudProviderGrid(
+                  title: 'Private Cloud',
+                  providers: kProviders.where((provider) => !provider.public),
+                ),
+                _CloudProviderGrid(
+                  title: 'Public Cloud',
+                  providers: kProviders.where((provider) => provider.public),
+                ),
+              ],
             ),
-            const SizedBox(height: kSpacing),
-            const Text('Where do you want your apps stack to run?'),
-            const Spacer(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: kSpacing),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  for (var i = 0; i < kProviders.length; ++i)
-                    Expanded(
-                      child: _CloudProviderButton(
-                        asset: kProviders[i].asset,
-                        title: kProviders[i].title,
-                        onPressed: () =>
-                            Navigator.of(context).pushNamed(Routes.coffee),
-                      ),
-                    ),
-                ],
-              ),
-            ),
-            const SizedBox(height: kSpacing),
-            const Spacer(),
-          ],
-        ),
+          ),
+          const SizedBox(height: kSpacing),
+        ],
       ),
     );
   }
@@ -65,30 +62,58 @@ class CloudScreen extends StatelessWidget {
 class _CloudProviderButton extends StatelessWidget {
   const _CloudProviderButton({
     Key? key,
-    required this.asset,
-    required this.title,
+    required this.vendor,
     required this.onPressed,
   }) : super(key: key);
 
-  final String asset;
-  final String title;
+  final String vendor;
   final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(kSpacing),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          child: SvgPicture.asset('assets/$vendor.svg', fit: BoxFit.contain),
+        ),
+      ),
+    );
+  }
+}
+
+class _CloudProviderGrid extends StatelessWidget {
+  const _CloudProviderGrid({
+    Key? key,
+    required this.title,
+    required this.providers,
+  }) : super(key: key);
+
+  final String title;
+  final Iterable<CloudProvider> providers;
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
+      mainAxisSize: MainAxisSize.min,
       children: <Widget>[
         Text(title),
-        AspectRatio(
-          aspectRatio: 1,
-          child: Padding(
-            padding: const EdgeInsets.all(kSpacing),
-            child: ElevatedButton(
-              onPressed: onPressed,
-              child: asset.endsWith('.svg')
-                  ? SvgPicture.asset('assets/$asset', fit: BoxFit.contain)
-                  : Image.asset('assets/$asset', fit: BoxFit.contain),
-            ),
+        const SizedBox(height: kSpacing / 2),
+        Expanded(
+          child: GridView.count(
+            shrinkWrap: true,
+            scrollDirection: Axis.horizontal,
+            crossAxisCount: 2,
+            children: providers.map((provider) {
+              return _CloudProviderButton(
+                vendor: provider.vendor,
+                onPressed: () {
+                  Navigator.of(context).pushNamed(Routes.coffee);
+                },
+              );
+            }).toList(),
           ),
         ),
       ],
